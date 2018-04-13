8fbc950fecf2ea0d99c85eaf3ee927518dc9e572
Ricardo Sanz <ricardo.sanz@upm.es>
2018-04-13 16:33:12 +0200

    Add hello.c

---
+++ Projects/HelloWorld/hello.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : hello.c
+ Author      : Ricardo Sanz
+ Version     : 1.0
+ Copyright   : (C) copyright rsanz.com
+ Description : Hello World in C, STD-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World !"); /* prints Hello World ! */
+	return EXIT_SUCCESS;
+}
